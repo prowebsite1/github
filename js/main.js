4f1eb9ecd102a9608864f7e9c9f004bd18c278fd
prowebsite1 <75897929+prowebsite1@users.noreply.github.com>
2020-12-20 20:24:50 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function (){
+
+    $("#btn").click(function(){
+        document.body.style.backgroundColor='#383838';
+    });
+
+    $("#btn").click(function(){
+        document.body.style.color='white';
+    });
+});
